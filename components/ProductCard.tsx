72320e6946e3936d954f9a0ef7d51082b65796dc
joshuabelt <belmarjosdan07@gmail.com>
2026-07-24 11:07:41 -0600

    Agregar imagenes y corregir su posición

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import type { Product } from "../types/Product";
 import { useAppDispatch } from "../redux/hooks";
 import { addToCart } from "../redux/features/cart/cartSlice";
@@ -14,12 +15,23 @@ export default function ProductCard({ product }: ProductCardProps) {
 
   return (
     <div className={styles.card}>
-      <h3 className={styles.title}>{product.title}</h3>
-      <p className={styles.price}>${product.price}</p>
-
-      <button className={styles.button} onClick={() => dispatch(addToCart(product))}>
-        Agregar al carrito
-      </button>
+      <div className={styles.imageContainer}>
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={150}
+          height={150}
+          className={styles.image}
+          priority={false}
+        />
+      </div>
+      <div className={styles.content}>
+        <h3 className={styles.title}>{product.title}</h3>
+        <p className={styles.price}>${product.price}</p>
+        <button className={styles.button} onClick={() => dispatch(addToCart(product))}>
+          Agregar al carrito
+        </button>
+      </div>
     </div>
   );
 }
--- data/products.ts
+++ data/products.ts
@@ -5,7 +5,7 @@ export const products: Product[] = [
     id: 1,
     title: "Mouse inalambrico Bluetooth Lenovo Yoga Tidal Teal Silencioso Gy51s61918",
     price: 19.95,
-    image: "/images/product1.jpg",
+    image: "/mouse.jpg",
     category: "Computo",
     quantity: 10
   },
@@ -13,7 +13,7 @@ export const products: Product[] = [
     id: 2,
     title: "UPS Forza Interactivo Nt-1201pro 1200va 600w 8 Salidas Nema 5-15p",
     price: 99.95,
-    image: "/images/product2.jpg",
+    image: "/ups forza.jpg",
     category: "Computo",
     quantity: 15
   },
@@ -21,7 +21,7 @@ export const products: Product[] = [
     id: 3,
     title: "Teclado Usb Genius Slimstar 230ii Negro",
     price: 29.99,
-    image: "/images/product2.jpg",
+    image: "/teclado.jpg",
     category: "Computo",
     quantity: 5
   },
@@ -29,7 +29,7 @@ export const products: Product[] = [
     id: 4,
     title: "Laptop lenovo ideapda slim3 15arp10 proc. amd ryzen 7 170 ram 16gb almacenamiento ssd 512gb pantalla tactil fhd+ 15.3p w11h luna grey 83k700tuus",
     price: 999.95,
-    image: "/images/product2.jpg",
+    image: "/laptop 1.jpg",
     category: "Computo",
     quantity: 20
   },
@@ -37,7 +37,7 @@ export const products: Product[] = [
     id: 5,
     title: "Laptop asus vivobook f1605va-ws96 proc. intel core i9 13900h ram ddr4 16gb almacenamiento ssd 1tb pantalla wuxga 16p w11h kb us silver",
     price: 1099.95,
-    image: "/images/product2.jpg",
+    image: "/laptop 2.jpg",
     category: "Computo",
     quantity: 10
   },
@@ -45,7 +45,7 @@ export const products: Product[] = [
     id: 6,
     title: "Headset inalambrico bt redmi neo negro hi-res audio 73670",
     price: 69.95,
-    image: "/images/product2.jpg",
+    image: "/audifonos 1.jpg",
     category: "Electronica",
     quantity: 13
   },
@@ -53,7 +53,7 @@ export const products: Product[] = [
     id: 7,
     title: "Monitor asus tuf vg259q5a panel fast ips fhd 24.5p 200hz 1ms gtg hdmi dp",
     price: 149.95,
-    image: "/images/product2.jpg",
+    image: "/Monitor asus tuf vg259q5a panel fast ips fhd 24.5p 200hz 1ms gtg hdmi dp.jpg",
     category: "Computo",
     quantity: 10
   },
@@ -61,7 +61,7 @@ export const products: Product[] = [
     id: 8,
     title: "Consola nintendo switch2 bundle choose one esd pantalla fhd 7.9p hdr hasta 120hz almacenamiento 256gb",
     price: 769.95,
-    image: "/images/product2.jpg",
+    image: "/Consola nintendo switch2 bundle choose one esd pantalla fhd 7.9p hdr hasta 120hz almacenamiento 256gb.jpg",
     category: "Gamer",
     quantity: 15
   },
@@ -69,7 +69,7 @@ export const products: Product[] = [
     id: 9,
     title: "Control sony dualsense para playstation 5 cfi-zct2 negro medianoche 23227",
     price: 89.95,
-    image: "/images/product2.jpg",
+    image: "/Control sony dualsense para playstation 5 cfi-zct2 negro medianoche 23227.jpg",
     category: "Gamer",
     quantity: 5
   },
@@ -77,7 +77,7 @@ export const products: Product[] = [
     id: 10,
     title: "Consola sony playstation 5 pro digital 2tb pssr hdr 4k",
     price: 1299.00,
-    image: "/images/product2.jpg",
+    image: "/Consola sony playstation 5 pro digital 2tb pssr hdr 4k.jpg",
     category: "Gamer",
     quantity: 10
   },
@@ -85,7 +85,7 @@ export const products: Product[] = [
     id: 11,
     title: "Silla profesional ergonomica thunder x3 xtc mesh chroma azul y negro hasta 125kg",
     price: 289.95,
-    image: "/images/product2.jpg",
+    image: "/Silla profesional ergonomica thunder x3 xtc mesh chroma azul y negro hasta 125kg.jpg",
     category: "Home Office",
     quantity: 5
   },
@@ -93,7 +93,7 @@ export const products: Product[] = [
     id: 12,
     title: "Fuente de poder msi mag a750gn pcie5 750w 80+gold",
     price: 119.95,
-    image: "/images/product2.jpg",
+    image: "/Fuente de poder msi mag a750gn pcie5 750w 80+gold.jpg",
     category: "Computo",
     quantity: 5
   },
@@ -101,7 +101,7 @@ export const products: Product[] = [
     id: 13,
     title: "Disipador de aire deepcool ak400 g2 digital nyx para intel 1851 y amd am5 negro r-ak400g2-bknnmn-gjd-1",
     price: 59.95,
-    image: "/images/product2.jpg",
+    image: "/Disipador de aire deepcool ak400 g2 digital nyx para intel 1851 y amd am5 negro r-ak400g2-bknnmn-gjd-1.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -109,7 +109,7 @@ export const products: Product[] = [
     id: 14,
     title: "Motherboard gigabyte b860m k gen5 lga1851 ddr5",
     price: 149.95,
-    image: "/images/product2.jpg",
+    image: "/Motherboard gigabyte b860m k gen5 lga1851 ddr5.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -117,7 +117,7 @@ export const products: Product[] = [
     id: 15,
     title: "Motherboard msi pro a620am-b evo amd am5 ddr5 rj45 2.5g 64mb bios",
     price: 119.95,
-    image: "/images/product2.jpg",
+    image: "/Motherboard msi pro a620am-b evo amd am5 ddr5 rj45 2.5g 64mb bios.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -125,7 +125,7 @@ export const products: Product[] = [
     id: 16,
     title: "Procesador intel core ultra 7 270k plus 8p+16e+24t 40m lga1851 requiere disipador termico",
     price: 449.95,
-    image: "/images/product2.jpg",
+    image: "/Procesador intel core ultra 7 270k plus 8p+16e+24t 40m lga1851 requiere disipador termico.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -133,7 +133,7 @@ export const products: Product[] = [
     id: 17,
     title: "Sistema de enfriamiento liquido redragon ccw-1019 240mm rgb lga1700 am5 negro",
     price: 64.95,
-    image: "/images/product2.jpg",
+    image: "/Sistema de enfriamiento liquido redragon ccw-1019 240mm rgb lga1700 am5 negro.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -141,7 +141,7 @@ export const products: Product[] = [
     id: 18,
     title: "Memoria ram hiksemi ddr4 8gb 2666mhz laptop hsc408s26z1",
     price: 99.95,
-    image: "/images/product2.jpg",
+    image: "/Memoria ram hiksemi ddr4 8gb 2666mhz laptop hsc408s26z1.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -149,7 +149,7 @@ export const products: Product[] = [
     id: 19,
     title: "Memoria ram kingston ddr5 8gb 5600mhz laptop kvr56s46bs6-8",
     price: 99.95,
-    image: "/images/product2.jpg",
+    image: "/Memoria ram kingston ddr5 8gb 5600mhz laptop kvr56s46bs6-8.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -157,7 +157,7 @@ export const products: Product[] = [
     id: 20,
     title: "Memoria ram ddr4 kingston fury beast 32gb 3200mt/s rgb kf432c16bb2a/32 pc",
     price: 399.95,
-    image: "/images/product2.jpg",
+    image: "/Memoria ram ddr4 kingston fury beast 32gb 3200mts rgb kf432c16bb2a32 pc.jpg",
     category: "Hardware",
     quantity: 5
   },
@@ -165,7 +165,7 @@ export const products: Product[] = [
     id: 21,
     title: "Laptop asus vivobook x1407qa-v14.x116512 proc. snapdragon x x126100 (8cores) ram ddr5 16gb almacenamiento ssd 512gb pantalla wuxga 14p w11h azul quiet",
     price: 799.95,
-    image: "/images/product2.jpg",
+    image: "/Laptop asus vivobook x1407qa-v14.x116512 proc. snapdragon x x126100 (8cores) ram ddr5 16gb almacenamiento ssd 512gb pantalla wuxga 14p w11h azul quiet.jpg",
     category: "Computo",
     quantity: 5
   },
@@ -173,7 +173,7 @@ export const products: Product[] = [
     id: 22,
     title: "Laptop hp 15-fd0557nr proc. intel core i7 1255u ram ddr4 12gb almacenamiento ssd 512gb pantalla fhd ips 15.6p w11 bm7b8ua",
     price: 899.95,
-    image: "/images/product2.jpg",
+    image: "/Laptop hp 15-fd0557nr proc. intel core i7 1255u ram ddr4 12gb almacenamiento ssd 512gb pantalla fhd ips 15.6p w11 bm7b8ua.jpg",
     category: "Computo",
     quantity: 5
   },
@@ -181,7 +181,7 @@ export const products: Product[] = [
     id: 23,
     title: "Memoria usb kingston 256gb exodia s dtxg2/256gb usb 3.2",
     price: 29.95,
-    image: "/images/product2.jpg",
+    image: "/Memoria usb kingston 256gb exodia s dtxg2256gb usb 3.2.png",
     category: "Computo",
     quantity: 5
   },
@@ -189,7 +189,7 @@ export const products: Product[] = [
     id: 24,
     title: "Switch hdmi bidireccional 2 entradas/salidas, 1salida/entrada xtech xha-440",
     price: 12.95,
-    image: "/images/product2.jpg",
+    image: "/Switch hdmi bidireccional 2 entradasysalidas, 1salidayentrada xtech xha-440.jpg",
     category: "Electronica",
     quantity: 5
   },
@@ -197,7 +197,7 @@ export const products: Product[] = [
     id: 25,
     title: "Adaptador de salida de video usb-c a hdmi maxell 347890",
     price: 7.95,
-    image: "/images/product2.jpg",
+    image: "/Adaptador de salida de video usb-c a hdmi maxell 347890.jpg",
     category: "Electronica",
     quantity: 5
   },
@@ -205,7 +205,7 @@ export const products: Product[] = [
     id: 26,
     title: "Router nexxt bolt ncm-x3000 wifi6 dualband rj45 x4 gigabit 2pack",
     price: 49.95,
-    image: "/images/product2.jpg",
+    image: "/Router nexxt bolt ncm-x3000 wifi6 dualband rj45 x4 gigabit 2pack.jpg",
     category: "Redes",
     quantity: 5
   },
@@ -213,7 +213,7 @@ export const products: Product[] = [
     id: 27,
     title: "Hub usb-a ugreen 20265 rj45 usb-a x3",
     price: 17.95,
-    image: "/images/product2.jpg",
+    image: "/Hub usb-a ugreen 20265 rj45 usb-a x3.jpg",
     category: "Redes",
     quantity: 5
   },
@@ -221,7 +221,7 @@ export const products: Product[] = [
     id: 28,
     title: "Tarjeta de red pci-e rj45 ugreen gigabit 30771",
     price: 9.95,
-    image: "/images/product2.jpg",
+    image: "/Tarjeta de red pci-e rj45 ugreen gigabit 30771.jpg",
     category: "Redes",
     quantity: 5
   },
@@ -229,7 +229,7 @@ export const products: Product[] = [
     id: 29,
     title: "Nvr vigi tp-link 8 canales nvr1008h onvif h.265+ hasta 10tb hdd",
     price: 179.95,
-    image: "/images/product2.jpg",
+    image: "/Nvr vigi tp-link 8 canales nvr1008h onvif h.265+ hasta 10tb hdd.jpg",
     category: "Redes",
     quantity: 5
   },
@@ -237,7 +237,7 @@ export const products: Product[] = [
     id: 30,
     title: "Router portatil mercusys mt110 4g lte hasta 10 usuarios hasta 10h",
     price: 29.99,
-    image: "/images/product2.jpg",
+    image: "/Router portatil mercusys mt110 4g lte hasta 10 usuarios hasta 10h.jpg",
     category: "Redes",
     quantity: 5
   }
